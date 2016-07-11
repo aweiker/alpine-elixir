7f7ce0e89a727e7dba8e6b90121e658c17affcb6
Aaron Weiker <aaron@weiker.org>
2016-07-11 10:43:12 -0700

    Adding in xmerl

--- dev/1.3.1/build.sh
+++ dev/1.3.1/build.sh
@@ -3,9 +3,8 @@
 set -e pipefail
 VERSION="${PWD##*/}"
 
-rm Precompiled.zip*
 wget https://github.com/elixir-lang/elixir/releases/download/v$VERSION/Precompiled.zip
-unzip -d elixir Precompiled.zip
+unzip -o -d elixir Precompiled.zip
 
 docker build -t aweiker/elixir-dev:$VERSION .
 rm -Rf elixir
